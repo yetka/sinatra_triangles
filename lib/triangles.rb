1fd504c8c9840721b15966b6ea50146e4335b66a
Kevin Babcock and Malgorzata Haniszewska <malgorzata@>
2017-11-27 13:04:31 -0800

    fix problems with lib file by converting string to an integer

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,8 +1,8 @@
 class Triangle
   def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    @side1 = side1.to_i
+    @side2 = side2.to_i
+    @side3 = side3.to_i
   end
 
   def triangle?
